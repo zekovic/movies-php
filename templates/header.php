dc3c65ea0831533d5c06f3c3e97d4c3826eed91e
Mihajlo Zeković <neparkiraj@gmail.com>
2024-01-12 00:34:06 +0100

    Favicon

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?= GlobVars::$site_name ?></title>
+<link rel="icon" type="image/x-icon" href="/assets/img/favicon.png">
 <script type="text/javascript" src="/assets/jquery-3.7.1.min.js"></script>
 <script type="text/javascript" src="/assets/jquery-ui.min.js"></script>
 
